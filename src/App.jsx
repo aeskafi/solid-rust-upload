881453ff7719265d71a6d1d90a0e4f108d267236
aeskafi <arham.eskafi@gmail.com>
2022-05-18 11:15:31 +0300

    Remove boilerplates.

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,8 @@ import styles from './App.module.css';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
